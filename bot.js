51ab47f1cb23428fb75807f67e56d459a706d28b
Bioblitz <bioblitz.github.com>
2019-08-02 22:43:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -86,7 +86,7 @@ client.on('message', (message)=>{
 
 function kaisa(){
     var fs = require('fs');
-    fs.readFile('text.txt', function(err, buf) {   
+    fs.readFile('temp.txt', function(err, buf) {   
          var lines = buf.toString().split('\n');
          var randomizer = [Math.floor(Math.random()*lines.length)];
          return randomizer;
